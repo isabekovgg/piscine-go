4ccd8c373eb9c4cb5bb4f5d9e806608000641acc
Your Name <isabekovgg@gmail.com>
2019-10-16 02:06:57 +0600

    second

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,8 +4,8 @@ import ("fmt"
 "github.com/01-edu/z01"	
 )
 func main() { 
-	for i :=97; i <=122; i++ {
+	for i :=97; i <=121; i++ {
 		fmt.Printf("%c",i)
 	}
-	fmt.Printf("\n")
+	fmt.Println("c", 122)
 }
