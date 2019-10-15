d657711ec1502b93f4c16a2a0e1b96eb3ed59d89
Your Name <isabekovgg@gmail.com>
2019-10-16 03:30:16 +0600

    lallaaa

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,11 +1,10 @@
 package main
 
-import (
-	"github.com/01-edu/z01"	
-)
+import "github.com/01-edu/z01"	
+
 func main() { 
 	for i :=97; i <=122; i++ {
-		z01.PrintRune("%c",i)
+		z01.PrintRune(i)
 	}
 	z01.PrintRune("\n")
 }
