f1e39fef7afed1dfc16417d29f210b3c5c43feda
Your Name <isabekovgg@gmail.com>
2019-10-16 10:18:17 +0600

    dsadasdasdasda

---
+++ printcomb2.go
@@ -0,0 +1,31 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+		for a := '0'; a <= '9'; a++ {
+			for b := '0'; b <= '9'; b++ {
+				for c := '0'; c <= '9'; c++ {
+					for d := '0'; d <='9' d++ {
+						if c > a {
+							z01.PrintRune(a)
+							z01.PrintRune(b)
+							z01.PrintRune('')
+							z01.PrintRune(c)
+							z01.PrintRune(d)
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						} if else c == a && d > b {
+							z01.PrintRune(a)
+							z01.PrintRune(b)
+							z01.PrintRune('')
+							z01.PrintRune(c)
+							z01.PrintRune(d)
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
+			}
+		}
+}
