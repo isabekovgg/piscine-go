b75e2f4d1b4f7cdffa04000ef0a03dcc6028b110
Your Name <isabekovgg@gmail.com>
2019-10-16 09:46:09 +0600

    dsadasdasdadadsa

--- printcomb.go
+++ printcomb.go
@@ -7,13 +7,13 @@ func PrintComb() {
 		for x := '1'; x <= '8'; x++ {
 			for y := '2'; y <= '9'; y++ {
 				if i < x && x < y {
-						z01.PrintRune(i)
-						z01.PrintRune(x)
-						z01.PrintRune(y)
-						if i !=7 && x !=8{
+					z01.PrintRune(i)
+					z01.PrintRune(x)
+					z01.PrintRune(y)
+					if i !=7 && x !=8{
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
-						}
+					}
 				}
 			}
 		}
