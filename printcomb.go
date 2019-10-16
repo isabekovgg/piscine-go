2813d6233969a182c46a60ae8ef7168088c1ad3f
Your Name <isabekovgg@gmail.com>
2019-10-16 09:05:13 +0600

    dsadasdasda

--- printcomb.go
+++ printcomb.go
@@ -5,14 +5,14 @@ import "github.com/01-edu/z01"
 func PrintComb() {
 	for i := '0'; i <= '7'; i++ {
 		for x := '1'; x <= '8'; x++ {
-			for y := '2'; y<= '9'; y++ {
+			for y := '2'; y <= '9'; y++ {
 				if i < x {
 					if x < y {
-					z01.PrintRune(i)
-					z01.PrintRune(x)
-					z01.PrintRune(y)
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+						z01.PrintRune(i)
+						z01.PrintRune(x)
+						z01.PrintRune(y)
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 						
 					}
 				}
