0275bb2035b1ebe61cad2f40e3ee9671493c9011
Your Name <isabekovgg@gmail.com>
2019-10-17 18:52:07 +0600

    dsadada

--- pointone.go
+++ pointone.go
@@ -7,8 +7,8 @@ func PointOne(n *int) {
 	*n = 1
 }
 
-	// func main() {
-	// 	n := 0
-	// 	PointOne(&n)
-	// 	fmt.Println(n)
-	// }
+// func main() {
+// 	n := 0
+// 	PointOne(&n)
+// 	fmt.Println(n)
+// }
