31a4d681a9d57b49db8926a088a29bc1d3648d75
Your Name <isabekovgg@gmail.com>
2019-10-17 19:09:39 +0600

    div

---
+++ ultimatedivmod.go
@@ -0,0 +1,22 @@
+package piscine
+
+// import "fmt"
+
+func UltimateDivMod (a *int, b *int) {
+
+	var x int;
+	var y int;
+	x = *a / *b
+	y = *a % *b
+	*a = x
+	*b = y
+
+}
+
+// func main() {
+// 	a := 13
+// 	b := 2
+// 	UltimateDivMod(&a, &b)
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+// }
