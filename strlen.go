db52eb1ff72031cafc88bedb7afeb497abc3ae28
Your Name <isabekovgg@gmail.com>
2019-10-17 20:41:48 +0600

    sdaasodkkqwe

--- strlen.go
+++ strlen.go
@@ -1,7 +1,5 @@
 package piscine
 
-import "fmt"
-
 func StrLen(str string) int {
 	y := 0
 	for range str {
@@ -9,9 +7,3 @@ func StrLen(str string) int {
 	}
 	return y
 }
-
-// func main() {
-// 	str := "Hello World!"
-// 	nb := StrLen(str)
-// 	fmt.Println(nb)
-// }
