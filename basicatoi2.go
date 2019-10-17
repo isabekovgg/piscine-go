6fea1a074f9e0aabf7de3517a538358d2860aadf
Your Name <isabekovgg@gmail.com>
2019-10-18 00:29:26 +0600

    dkoqwe

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,6 +1,6 @@
-package main
+package piscine
 
-import "fmt"
+// import "fmt"
 
 func BasicAtoi2(s string) int {
 	stringrune := []rune(s)
@@ -10,29 +10,29 @@ func BasicAtoi2(s string) int {
 		y++
 	}
 	for x := 0; x <= y-1; x++ {
-		if int(stringrune[x]-'0') >= 0 && int(stringrune[x]-'0') < 10{
+		if int(stringrune[x]-'0') >= 0 && int(stringrune[x]-'0') < 10 {
 		final = final*10 + int(stringrune[x]-'0')
-		}else {
+		} else {
 			return 0
 		}
 	}
 	return final
 }
 
-func main() {
-	s := "12345"
-	s2 := "0000000012345"
-	s3 := "012 345"
-	s4 := "Hello World!"
+// func main() {
+// 	s := "12345"
+// 	s2 := "0000000012345"
+// 	s3 := "012 345"
+// 	s4 := "Hello World!"
 
-	n := BasicAtoi2(s)
-	n2 := BasicAtoi2(s2)
-	n3 := BasicAtoi2(s3)
-	n4 := BasicAtoi2(s4)
+// 	n := BasicAtoi2(s)
+// 	n2 := BasicAtoi2(s2)
+// 	n3 := BasicAtoi2(s3)
+// 	n4 := BasicAtoi2(s4)
 
-	fmt.Println(n)
-	fmt.Println(n2)
-	fmt.Println(n3)
-	fmt.Println(n4)
+// 	fmt.Println(n)
+// 	fmt.Println(n2)
+// 	fmt.Println(n3)
+// 	fmt.Println(n4)
 
-}
+// }
