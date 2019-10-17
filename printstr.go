85d2882579030d51885eac0a6306518d3968c16f
Your Name <isabekovgg@gmail.com>
2019-10-17 19:34:42 +0600

    sadasdasdasdasdasda

---
+++ printstr.go
@@ -0,0 +1,14 @@
+package piscine
+
+// import "fmt"
+
+func PrintStr(str string) {
+	for i := 0; i < len(str); i++ {
+	fmt.Printf(string(str[i]))
+	}
+}
+
+// func main() {
+// 	str := "Hello World!"
+// 	PrintStr(str)
+// }
