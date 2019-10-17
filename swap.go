2dc41db64ce79f6c7ec743cd2d8c11b995204e16
Your Name <isabekovgg@gmail.com>
2019-10-17 20:47:00 +0600

    dsadasdagew

--- swap.go
+++ swap.go
@@ -1,18 +1,8 @@
 package piscine
 
-import "fmt"
-
 func Swap(a *int, b *int) {
 	x := *a
 	y := *b
 	*b = x
 	*a = y
 }
-
-// func main() {
-// 	a := 0
-// 	b := 1
-// 	Swap(&a, &b)
-// 	fmt.Println(a)
-// 	fmt.Println(b)
-// }
