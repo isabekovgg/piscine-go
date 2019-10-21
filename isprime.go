00ca09d17858eb029f145a35a1df126fd446a244
Your Name <isabekovgg@gmail.com>
2019-10-21 21:09:04 +0600

    dsaij

---
+++ isprime.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func IsPrime(nb int) bool {
+
+	if nb > 1 {
+		for i := 2; i < nb; i++ {
+			if nb%i == 0 {
+				return false
+			}	
+		}
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	fmt.Println(IsPrime(5))
+	fmt.Println(IsPrime(4))
+}
