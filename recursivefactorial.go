abb7ed660f768b0b0872f3981c77d057beb23df4
Your Name <isabekovgg@gmail.com>
2019-10-21 13:32:03 +0600

    recursive

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,11 +3,12 @@ package piscine
 // import "fmt"
 
 func RecursiveFactorial(nb int) int {
-
-	if nb == 0 {
-		return 1
-	}
-	return nb * RecursiveFactorial(nb-1)
+	if nb > -20 && nb < 20 {
+		if nb == 0 {
+			return 1
+		}
+		return nb * RecursiveFactorial(nb-1)
+	}	
 }
 
 // func main() {
