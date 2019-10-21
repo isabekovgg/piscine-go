43eb3c0deebf56febe25827acdd35ef84b497df3
Your Name <isabekovgg@gmail.com>
2019-10-21 13:02:42 +0600

    coma

---
+++ iterativepower.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func IterativePower(nb int, power int) int {
+	if power > 0 {
+		result := 1
+		for x := 1; x <= power; x++ {
+			result = result*nb
+		}
+		return result
+	} else if power == 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	arg1 := 4
+	arg2 := 3
+	fmt.Println(IterativePower(arg1, arg2))
+}
---
+++ recursivepower.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func RecursivePower(nb int, power int) int {
+
+	if power == 0 {
+		return 1
+	}
+	return nb*RecursivePower(nb, power-1)
+}
+
+func main() {
+	arg1 := 4
+	arg2 := 3
+	fmt.Println(RecursivePower(arg1, arg2))
+}
