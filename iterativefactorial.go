5b9ae4538eedec561f218ca58d1403612dd56698
Your Name <isabekovgg@gmail.com>
2019-10-21 11:43:45 +0600

    dsajirqw

---
+++ iterativefactorial.go
@@ -0,0 +1,16 @@
+package piscine
+
+import "fmt"
+
+func IterativeFactorial(nb int) int {
+	result := 1
+	for x := 1; x <= nb; x++ {
+		result = result * x
+	}
+	return result
+}
+
+// func main() {
+// 	arg := 3
+// 	fmt.Println(IterativeFactorial(arg))
+// }
