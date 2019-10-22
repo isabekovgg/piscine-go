2ea91f8bffd8c3615ae0c4adb7ad2382717582c0
Your Name <isabekovgg@gmail.com>
2019-10-22 20:38:59 +0600

    dqkwi

--- nrune.go
+++ nrune.go
@@ -3,9 +3,18 @@ package piscine
 // import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
+	if n <= slen(s) {
+		t := []rune(s)
+		return t[n-1]
+	}
+}
 
-	t := []rune(s)
-	return t[n-1]
+func slen(s string) int {
+	y := 0
+	for range s {
+		y++
+	}
+	return y
 }
 
 // func main() {
