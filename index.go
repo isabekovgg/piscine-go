a48f8060bac8bfa406168050a1418f8c7c7bedde
Your Name <isabekovgg@gmail.com>
2019-10-22 20:46:00 +0600

    dwqki

---
+++ index.go
@@ -0,0 +1,30 @@
+package piscine
+
+// import (
+// 	"fmt"
+// )
+
+func Index(s string, toFind string) int {
+	var x int
+	str := []rune(s)
+	sub := []rune(toFind)
+	y := 0
+	for range s {
+		y++
+	}
+	for i := 0; i < y; i++ {
+		if str[i] == sub[0] {
+			x = i
+			break
+		} else {
+			x = -1
+		}
+	}
+	return x
+}
+
+// func main() {
+// 	fmt.Println(Index("Hello!", "l"))
+// 	fmt.Println(Index("Salut!", "alu"))
+// 	fmt.Println(Index("Ola!", "hOl"))
+// }
