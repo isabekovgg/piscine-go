b7bb2002c3fd0df2a08927d80cd941f10c0af81c
Your Name <isabekovgg@gmail.com>
2019-10-22 23:18:27 +0600

    dkqwie

--- capitalize.go
+++ capitalize.go
@@ -16,7 +16,7 @@ func Capitalize(s string) string {
 			new[0] = (new[0] - 32)
 		}
 		if i > 0 {
-			if (new[i] > 'Z' && new[i] < 'a') || new[i-1] <= 'A' || new[i-1] >= 'z' {
+			if (new[i-1] > 'Z' && new[i-1] < 'a') || (new[i-1] < 'A' && new[i-1] > '9') || new[i-1] < '0' || new[i-1] > 'z' {
 				if new[i] >= 'a' && new[i] <= 'z' {
 					new[i] = (new[i] - 32)
 					i++
