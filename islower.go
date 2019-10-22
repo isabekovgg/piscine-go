5dce8515c3690942af0daecb89386d7192868690
Your Name <isabekovgg@gmail.com>
2019-10-22 23:40:57 +0600

    dkqoweql

---
+++ islower.go
@@ -0,0 +1,17 @@
+package piscine
+
+// import "fmt"
+
+func IsLower(str string) bool {
+	new := []rune(str)
+	y := 0
+	for range str {
+		y++
+	}
+	for i := 0; i < y; i++ {
+		if new[i] < 'a' || (new[i] > 'z' {
+			return false
+		}
+	}
+	return true
+}
