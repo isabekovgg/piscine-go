653e17e8253eff21bb7f8163de9fdc66dd365a3a
Your Name <isabekovgg@gmail.com>
2019-10-22 23:59:00 +0600

    dwqsdjqi

---
+++ concat.go
@@ -0,0 +1,13 @@
+package piscine
+
+// import "fmt"
+
+func Concat(str1 string, str2 string) string {
+	result := str1 + str2
+	return result
+}
+
+// func main() {
+// 	fmt.Println(Concat("Hello!", " How are you?"))
+
+// }
