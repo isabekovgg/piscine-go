807e49a2035879b65a7a1c3fee9cb639e0db35e4
Your Name <isabekovgg@gmail.com>
2019-10-22 12:43:18 +0600

    printnbr

---
+++ printnbr.go
@@ -0,0 +1,19 @@
+package piscine
+
+import "fmt"
+
+// import "fmt"
+
+// import "fmt"
+
+func PrintNbr(n int) {
+
+	fmt.Printf("%v", n)
+
+}
+
+// func main() {
+// 	PrintNbr(-123)
+// 	PrintNbr(0)
+// 	PrintNbr(123)
+// }
