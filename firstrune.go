06ce08545473771daa0fc81c6b102232c8ae7608
Your Name <isabekovgg@gmail.com>
2019-10-22 20:33:23 +0600

    dkqwi

---
+++ firstrune.go
@@ -0,0 +1,16 @@
+package piscine
+
+// import "github.com/01-edu/z01"
+
+func FirstRune(s string) rune {
+
+	t := []rune(s)
+	return t[0]
+}
+
+// func main() {
+// 	z01.PrintRune(FirstRune("Hello!"))
+// 	z01.PrintRune(FirstRune("Salut!"))
+// 	z01.PrintRune(FirstRune("Ola!"))
+// 	z01.PrintRune('\n')
+// }
