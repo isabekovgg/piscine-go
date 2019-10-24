ae0fbb7f246e091d1673589bcd9f3f886349f7bc
Your Name <isabekovgg@gmail.com>
2019-10-24 19:57:52 +0600

    dqkwoe

---
+++ makerange.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func MakeRange(min, max int) []int {
+	if max > min {
+		ans := make([]int, max-min)
+
+		for i := 0; i < max-min; i++ {
+			ans[i] = i + min
+		}
+		return ans
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	fmt.Println(MakeRange(5, 10))
+	fmt.Println(MakeRange(10, 5))
+}
